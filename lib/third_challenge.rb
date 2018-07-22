b4e29937f40127fd013cf74933fe5534efc51cf8
Sherzod Karimov <karimovj@gmail.com>
2018-07-22 18:04:17 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,7 +26,7 @@ def third_challenge
         :age => "51"
         },
       :heroine => {
-        :name => "Julier",
+        :name => "Juliet",
         :age => "15",
         :status => "alive"
         },
